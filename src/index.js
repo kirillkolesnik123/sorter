6506f3c9b65a6ead13622114ee304edb78f790e0
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-11 16:52:13 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+   this.funcCompare = (a,b) => {
+   return a - b;
+   }
   }
 
   add(element) {
-    // your implementation
+  
+    this.arr.push(element);
+  
   }
 
   at(index) {
     // your implementation
+     return this.arr[index];
   }
 
   get length() {
-    // your implementation
+      // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr
   }
 
   sort(indices) {
     // your implementation
+     indices.sort((a,b) => {
+   return a - b});
+
+    let temp = [];
+    for (let i in indices) {
+      temp.push(this.arr[indices[i]]);
+    }
+
+    temp.sort(this.funcCompare);
+
+    for(let i = 0; i<indices.length; i++){
+      this.arr.splice(indices[i], 1, temp[i]);
+    } 
   }
+
+
+  
 
   setComparator(compareFunction) {
     // your implementation
+     this.funcCompare = compareFunction;
   }
 }
 
